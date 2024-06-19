0c487fcbbf4a983870f19ab82304d7d6e0bf54a2
Muhammad Malik Ardiansyah <muhammad.malik01@gmail.com>
2024-06-19 23:43:51 +0700

    Transaction to another metamask account

--- src/About.js
+++ src/About.js
@@ -21,11 +21,18 @@ const Profile = () => {
             <div className = "row justify-content-center">
                 <div className = "col-md-4 text-center">
                     <br></br>
-                    <h2>Panduan Lengkap Tata Cara Pembayaran Tagihan Listrik Berbasis Blockchain</h2>
+                    <h2>Panduan Singkat Tata Cara Pembayaran Tagihan Listrik Berbasis Blockchain</h2>
                     <br></br>
                     <div className = "d-grid gap-2">
                         <img src="guide.jpg" class="img-thumbnail" alt="guide"></img>
-                    </div>                
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <p>1. Pergi ke Halaman "Bayar Listrik".</p>
+                    <p>2. Tekan tombol "Connect Metamask Account" agar situs web dapat terhubung dengan wallet anda.</p>
+                    <p>3. Pastikan anda telah menginstal extension MetaMask pada browser yang anda gunakan.</p>
+                    <p>4. Tekan tombol "Bayar" untuk melakukan pembayaran.</p>
+                    <p>5. Pastikan anda memiliki saldo yang cukup pada wallet anda agar transaksi dapat berhasil dilakukan</p>
                 </div>
             </div>
         </div>       
--- src/Teams.js
+++ src/Teams.js
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState, useRef } from 'react';
 import { ethers } from 'ethers';
 import { collection, getDocs } from "firebase/firestore";
+import Swal from 'sweetalert2'
 
 async function fetchName() {
   const querySnapshot = await getDocs(collection(db, "userinfo"))
@@ -50,12 +51,13 @@ const Profile = () => {
     const daya = ["R1/900 VA", "R1/1300 VA", "R1/2200 VA", "R2/3500 VA", "R2/5500 VA"];
     const random = Math.floor(Math.random() * daya.length);
     const getRandomInteger = (min, max) => {
-      min = Math.ceil(min)
-      max = Math.floor(max)
+      // min = Math.ceil(min)
+      // max = Math.floor(max)
     
       return Math.floor(Math.random() * (max - min)) + min
     }
-    const randomInteger = getRandomInteger(300000, 500000)
+    // Harga bayar listrik Rp200.000 - Rp500.000
+    const randomInteger = getRandomInteger(12.21, 30.53)
     const numberWithCommas = (value) => {
       return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     }
@@ -78,8 +80,20 @@ const Profile = () => {
           .then(result => {
             accountChanged([result[0]])
           })
+          setInterval(() => {
+          }, 5000)
+          Swal.fire({
+            title: "Good job!",
+            text: "MetaMask berhasil terhubung",
+            icon: "success"
+          });
       } else {
-        setErrorMessage('Install MetaMask please!!')
+        Swal.fire({
+          title: 'Error!',
+          text: 'Install Metamask Terlebih Dahulu!!!',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        })
       }
     }
   
@@ -93,6 +107,26 @@ const Profile = () => {
       window.ethereum.request({method: 'eth_getBalance', params: [String(accountAddress), "latest"]})
       .then(balance => {
         setUserBalance(ethers.utils.formatEther(balance));
+      })
+    }
+
+    var amount = ((numberWithCommas(randomInteger) / 3540.05).toFixed(4)).toString();
+    const EtherToWei = ethers.utils.parseUnits(amount,"ether");
+
+    console.log(typeof(defaultAccount.toString()))
+
+    async function sendTransaction(event) {
+      event.preventDefault();
+      let params = [{
+        from: defaultAccount.toString(),
+        to: "0xe9E99062038e14724f4d9CB3546afDf45808642e",
+        gas: Number(21000).toString(16),
+        gasPrice: Number(2500000).toString(16),
+        value: Number(EtherToWei).toString(16),
+      }]
+  
+      let result = await window.ethereum.request({method: "eth_sendTransaction", params}).catch((err) => {
+        console.log(err)
       })
     }
 
@@ -113,17 +147,18 @@ const Profile = () => {
                         <p>Daya: {daya[random]}</p>
                         <p>Tanggal: {today}</p> 
                         {/* Tarif/Daya: R1/000000900VA */}
-                        <p>Total Bayar: Rp{numberWithCommas(randomInteger)}</p>
-                        <button type = "submit" className = "submit-btn" onClick = {(e) => logoutUser(e)}>Bayar</button>
-                        <br></br>
-                        <br></br>
+                        <p>Total Bayar: Rp{numberWithCommas(randomInteger*16400)}</p>
+                        {/* <button type = "submit" className = "submit-btn" onClick = {(e) => logoutUser(e)}>Bayar</button> */}
+                        {/* <br></br>
+                        <br></br> */}
                         <button onClick={connectWallet}>Connect Metamask Account</button>
                         <br></br>
                         <br></br>
                         <p>Address: {defaultAccount}</p>
                         <p>Balance:  {userBalance}</p>
-                        <br></br>
-                        <br></br>
+                        <form onSubmit={sendTransaction}>
+                          <input className = "bayar" type="submit" value="Bayar"/>
+                        </form>
                     </div>             
                 </div>
             </div>
